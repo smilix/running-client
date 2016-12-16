a5e083afba7760d888c22bd75e80ba14a86e38a0
Holger Cremer <HolgerCremer@gmail.com>
2016-12-16 21:59:30 +0100

    shows the date part in formatted time

--- src/app/shared/duration.pipe.ts
+++ src/app/shared/duration.pipe.ts
@@ -20,12 +20,17 @@ export class DurationPipe implements PipeTransform {
 
     var minutes = Math.floor(seconds / 60); // 60
     var hours = Math.floor(minutes / 60); // 1
+    var days = Math.floor(hours / 24);
 
     var secondsPart = Math.floor(seconds % 60);
     var minutesPart = Math.floor(minutes % 60);
     var hourPart = Math.floor(hours % 60);
+    var dayPart = Math.floor(days % 24);
 
     var result = '';
+    if (dayPart > 0) {
+      result += dayPart + 'd ';
+    }
     if (hourPart > 0) {
       result += DurationPipe.zeroPad(hourPart) + ':';
     }
